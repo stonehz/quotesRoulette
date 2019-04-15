2fee069fc2ff55800973d837e9516ea9d013807e
Antonios N <stonehz@gmail.com>
2019-04-15 13:14:16 +0100

    adds structure and multiple components for header main footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <Header />
+    </header>
+    <main>
+      <MainPage msg="Welcome to Your Vue.js App"/>
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import MainPage from './components/layout/MainPage.vue';
+import Header from './components/layout/Header.vue';
+import Footer from './components/layout/Footer.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Header,
+    MainPage,
+    Footer,
   },
 };
 </script>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1> Logo </h1>
+    <img alt="Vue logo" src="../../assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+
+<style>
+
+</style>
+
